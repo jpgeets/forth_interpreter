ba243db49fd20ac06c54a98529f7a7218b389c95
Jean-Pierre Geets <jp.partir@gmail.com>
2026-05-17 20:27:59 +0100

    Correct integration test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -6,8 +6,7 @@ def test_empty_input_returns_empty_stack():
     result = interpreter.execute("   ")
     assert result == []
 
-
 def test_comments_do_not_affect_execution():
     interpreter = Interpreter()
-    result interpreter.execute("6 2 + ( add ) 3 +")
+    result = interpreter.execute("6 2 + ( add ) 3 +")
     assert  result == [11]
